f4f53f00907c47b22e9be98c06a6111d4f5b12b9
moritzfelipe <stellmacher@gmail.com>
2018-11-15 12:19:25 +0100

    Update memeRegistry.sol

--- memeRegistry.sol
+++ memeRegistry.sol
@@ -1,35 +1,32 @@
 //Code for dacade dApp development 101 Events and finishing touches for the UI
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.24;
 
 contract memeRegistry {
-
+    
     struct memecert {
-        address creator;
         string name;
         uint timestamp;
     }
-
+    
     mapping (string => memecert) memecerts;
-
-    event memecertInfo(
-       address creator,
-       string name,
+    
+    event memecertInfo(      
+ 	string name,
        uint timestamp,
        string url
     );
-
-    function setMemecert(string _url, address _creator, string _name) public {
+    
+    function setMemecert(string _url, string _name) public {
         require (bytes(_url).length != 0);
-        require (_creator != 0x0);
         require (bytes(_name).length != 0);
-
-        require(memecerts[_url].creator == 0x0);
-
-        memecerts[_url] = memecert(_creator, _name, now);
-        emit memecertInfo(_creator, _name, now, _url);
+ 
+        require(memecerts[_url].timestamp == 0);
+        
+        memecerts[_url] = memecert(_name, now);
+        emit memecertInfo(_name, now, _url);
     }
-
-    function getMemecert(string _url) view public returns (address, string, uint) {
-        return (memecerts[_url].creator, memecerts[_url].name, memecerts[_url].timestamp);
+    
+    function getMemecert(string _url) view public returns (string, uint) {
+        return (memecerts[_url].name, memecerts[_url].timestamp);
     }
 }
